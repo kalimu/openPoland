495f200ce3e4032ceb92bb7866d56d2d5c28ae49
kalimu <kamil.wais@gmail.com>
2014-09-19 09:18:24 +0200

    subQuery in search function and documentation

--- R/openPolandSearch.R
+++ R/openPolandSearch.R
@@ -2,36 +2,52 @@
 #' 
 #' \code{openPolandSearch} returns a data frame with information of available datasets that were found with a given quary.
 #' 
+#' The query from \code{openPolandSearch} function is not case-sensitive.
+#' The token authorization is non-obligatory for database searching. If you want to use Polish diacritis in the query remeber to check if they are properly encoded ("UTF-8") before they are sent to API. If you use Polish localization the function \code{\link{enc2utf8}} should help.
+#' The query parameter is send to API, but the subQuery parameter is search locally after downloading the dataset lists from API.
+#' 
+#' @param query A character string.
+#' @param subQuery A characters string.
+#' @param token A character string. 
+#' @return A data table object with datasets ids and titles that have a query string inside them.
+#' @examples 
+#' \dontrun{
+#' openPolandSearch() # returns API demo list of datasets
+#' 
+#' openPolandSearch(query = 'telef')
+#' 
+#' openPolandSearch(query = "nauczy", subQuery = "akad")
+#' 
+#' # Polish diacritics are also available 
+#' openPolandSearch(query = enc2utf8('ludno\u015B\u0107'))
+#' }
 
+openPolandSearch = function (query = NULL, 
+                             token = NULL,
+                             subQuery = NULL) {
+    
+    url = "https://openpoland.net/api/list"
+    
+    if (!is.null(query)) {
+      
+        query = stringr::str_replace_all(query, pattern = " ", "+")
+        # title = str_replace_all(title, pattern = "-", "%2d")
 
+    }  
 
-    openPolandSearch = function (query = NULL, token = NULL) {
+    dt = openPolandAPI(url, query = enc2utf8(query), token = token)
+    
+    cat(NROW(dt)," datasets found from the query: '",query,"'.\n\n", sep = "")   
+    
+    df = as.data.frame(dt)
+    
+    if (!is.null(subQuery)) {
         
-        if (is.null(query)) {
-          
-            url = "https://openpoland.net/api/list"
-            
-        } else {
-            
-            query = stringr::str_replace_all(query, pattern = " ", "+")
-            # title = unlist(str_split(title, pattern = " ", ))
-            # title = str_replace_all(title, pattern = "-", "%2d")
-            
-            url = paste0("https://openpoland.net/api/list/")
-        }  
-
-        
-        
-        dt = openPolandAPI(url, query = enc2utf8(query), token = token)
-        
-        cat(NROW(dt)," datasets found from the query: '",query,"'.\n\n", sep = "")   
-        
-        
-        
-        #as.data.frame(
-             dt
-        #    )
-        # [str_detect(tolower(dt$title), title[2]),]
-        
+        df = df[stringr::str_detect(tolower(df$title),
+                                    tolower(subQuery[1])), ]
         
     }
+
+    df
+        
+}
