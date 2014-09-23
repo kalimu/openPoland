90963dfd41f70e4d9931df876c593ed558f77948
kalimu <kamil.wais@gmail.com>
2014-09-23 09:52:13 +0200

    openPolandData function docummentation

--- R/openPolandData.R
+++ R/openPolandData.R
@@ -1,48 +1,75 @@
-#' Search openPoland.net database for a specific dataset.
+#' Loads a full or filtered data from a selected dataset via openPoland API.
 #' 
-#' \code{openPolandSearch} returns a data frame with information of available datasets that were found with a given quary.
+#' \code{openPolandData} returns a data frame with data from a dataset with given ID. The data could be filtered by year or NTS id.
 #' 
+# The query from \code{openPolandSearch} function is not case-sensitive.
+# The token authorization is non-obligatory for database searching. 
+# Polish diacritics should also worked within a query.
+# The query parameter is send to API, but the subQuery parameter is search locally after downloading the dataset lists from API.
+# 
+#' @param id A character string.
+#' @param token A characters string. 
+#' @param nts A character string. 
+#' @param year A character string.
+#' 
+#' @return A data table object. The first column is a NTS id of territorial unit. The second column is a common name of territorial unit. Then there are from 1 to 5 columns with dimensions labels. All dimensions in a given dataset can be previewed by the \code{\link(openPolandMeta)} function. Last four columns of the data table are: year, measure unit, value and data attribute.
+#' 
+#' @seealso \code{\link{openPolandMeta}} for getting meta information about datasets, and \code{\link{openPolandSearch}} for searching database with a given query.
+# 
+#' @examples 
+#' \dontrun{
+#' 
+#' openPolandSearch("akad")
+#' #     id                                     title
+#' # 1 1836 Nauczyciele akademiccy (NTS-2, 1999-2012)
+#'
+#' # token for API authorization     
+#' token = "[alpha-numeric-string-you-get-after-registration-on-https://openpoland.net]"
+#' 
+#' # a fltered dataset with data for 1999 year in Central Region (NTS = 1000000000)
+#' openPolandData(id = 1836, token = token,nts = '1000000000', year='1999') 
+#' 
+#' # a whole, unfiltered dataset 
+#' openPolandData(id = 1836, token = token) 
+#' }
 
 
 
-    openPolandData = function (id, token, 
-                               nts = NULL,
-                               year = NULL
-                               ) {
+openPolandData = function (id = NULL, 
+                           token = NULL, 
+                           nts = NULL,
+                           year = NULL) {
+    
+    if (is.null(id)) {
+      
+        stop('\nWhat dataset "id" are you looking for?',
+             '\nUse openPolandSearch() function to search for a dataset.')
         
-        if (is.null(id)) {
-          
-            stop('What dataset "id" are you looking for?')
-            
-        } 
+    } 
+    
+    if (is.null(token)) {
         
-        if (is.null(token)) {
-            
-            stop('You need to send token for authorization!')
-            
-        }
+        stop('\nYou need to have a token for authorization!',
+             '\nRegister at https://openpoland.net/signup/ to get a token.')
         
-        id = as.numeric(id)
-
-        if (is.null(nts) & is.null(year)) {
-            
-            url = paste0("https://openPoland.net/api/asset/",id,"/data/")
-            
-            openPolandAPI(url, token)
-            
-        }  else {
-            
-            url = paste0("https://openPoland.net/api/asset/",id,"/data_filter/")
-            
-            openPolandAPI(url = url, 
-                          token = token, 
-                          nts = nts,
-                          year = year)
-                   
-        }
-
-
-        
- 
     }
+    
+    if (is.null(nts) & is.null(year)) {
+        
+        url = paste0("https://openPoland.net/api/asset/",id,"/data/")
+        
+        openPolandAPI(url, token)
+        
+    }  else {
+        
+        url = paste0("https://openPoland.net/api/asset/",id,"/data_filter/")
+        
+        openPolandAPI(url = url, 
+                      token = token, 
+                      nts = nts,
+                      year = year)
+               
+    }
+
+}
 
